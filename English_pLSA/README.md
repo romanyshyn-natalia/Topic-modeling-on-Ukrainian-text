c0d39d9b6eec47f0df358a6a53b6bf02c9b8c847
romanyshyn-natalia <romanyshyn.n@ucu.edu.ua>
2021-05-03 18:04:26 +0300

    lsa ukr with clustering: with errors

--- English_pLSA/README.md
+++ English_pLSA/README.md
@@ -1,2 +1,10 @@
 # pLSA on English texts
-...
+Example of pLSA on same English texts as for LSA.
+
+Example taken from https://github.com/yedivanseven/PLSA.
+
+```
+>>> import nltk
+>>> nltk.download('averaged_perceptron_tagger')
+>>> nltk.download('wordnet')
+```
---
+++ English_pLSA/english_plsa.py
@@ -0,0 +1,33 @@
+import sys
+import urllib.request
+import csv
+import matplotlib.pyplot as plt
+
+from plsa import Corpus, Pipeline, Visualize
+from plsa.pipeline import DEFAULT_PIPELINE
+from plsa.algorithms import PLSA
+
+csv_file = "data/Full-Economic-News-DFE-839861.csv"
+
+pipeline = Pipeline(*DEFAULT_PIPELINE)
+
+corpus = Corpus.from_csv(csv_file, pipeline)
+
+n_topics = 5
+plsa = PLSA(corpus, n_topics, True)
+
+result = plsa.fit()
+result = plsa.best_of(5)
+
+print(result.topic)
+
+new_doc = 'Hello! This is the federal humpty dumpty agency for state funding.'
+
+topic_components, number_of_new_words, new_words = result.predict(new_doc)
+
+print('Relative topic importance in new document:', topic_components)
+print('Number of previously unseen words in new document:', number_of_new_words)
+print('Previously unseen words in new document:', new_words)
+
+print(result.word_given_topic[0][:10])
+
