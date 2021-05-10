46474283233774359c5c47330e54c9dba8f156da
Daria Omelkina <odasha2001@gmail.com>
2021-05-10 18:00:37 +0300

    Added new graph

--- Ukrainian_TopicModeling/plsa_gensim.py
+++ Ukrainian_TopicModeling/plsa_gensim.py
@@ -78,50 +78,41 @@ def plot_clusters_with_topics(topics_matrix, clusters):
 
 
 if __name__ == "__main__":
-    x = range(2, 13, 1)
-    plt.plot(x, [0.24862516871891016, 0.3460672466169208, 0.31760168499772407, 0.3285116590501592, 0.33832683377588246,
-                 0.35241208177724, 0.38477830107102784, 0.3729613650152243, 0.3778253405113229, 0.3810417049391049,
-                 0.38329968618732657])
-    plt.xlabel("Number of Topics")
-    plt.ylabel("Coherence score")
-    plt.legend("coherence_values", loc='best')
-    plt.savefig('coherence_measure_graph_lemmatized.png')
-    # # For the first time using: uncomment code for downloading ua corpus in the preprocessor module
-    #
-    # # Uncomment to use preprocessed text with lemmatization
-    # # text = preprocess_with_lemmatization()
-    #
-    # # Use preprocessed text without lemmatization
-    # text = preprocess_normalized()
-    # dct = Dictionary(text[:2])
-    # print(dct.keys())
-    #
-    # # For that pLSA implementation, we will need to use each document not as a list, but as a string,
-    # # so we apply additional preparation
-    # prepared_text = prepare_preprocessed_text(text)  # 142822 words
-    #
-    # # Performing pLSa
-    # # TODO: uncomment
-    # # optimal_number_of_topics = 7
-    # optimal_number_of_topics = 2
-    #
-    # using_tf_idf = True
-    # pipeline = Pipeline(tokenize)
-    #
-    # result = perform_plsa(prepared_text, pipeline)
-    #
-    # # Visualization with the help of clusters and UMAP
-    # SOME_FIXED_SEED = 42
-    # # x_topics, tf_idf_sparse = get_vectorized_sparse_matrix(term_doc_matrix, dictionary, optimal_number_of_topics)
-    # # Creating sparse matrix
-    # dct = Dictionary(prepared_text)
-    # sparse = []
-    # for topic in result.word_given_topic:
-    #     sparse.append([])
-    #     for word_tuple in topic:
-    #         sparse[-1].append(word_tuple[0])
-    #
-    # # K-Means
-    # cluster_labels = create_k_means_model(optimal_number_of_topics, sparse)
-    # print(len(sparse), len(sparse[0]))
-    # # plot_clusters_with_topics(x_topics, cluster_labels)
+    # For the first time using: uncomment code for downloading ua corpus in the preprocessor module
+
+    # Uncomment to use preprocessed text with lemmatization
+    # text = preprocess_with_lemmatization()
+
+    # Use preprocessed text without lemmatization
+    text = preprocess_normalized()
+    dct = Dictionary(text[:2])
+
+    # For that pLSA implementation, we will need to use each document not as a list, but as a string,
+    # so we apply additional preparation
+    prepared_text = prepare_preprocessed_text(text)  # 142822 words
+
+    # Performing pLSa
+    # TODO: uncomment
+    # optimal_number_of_topics = 7
+    optimal_number_of_topics = 2
+
+    using_tf_idf = True
+    pipeline = Pipeline(tokenize)
+
+    result = perform_plsa(prepared_text, pipeline)
+
+    # Visualization with the help of clusters and UMAP
+    SOME_FIXED_SEED = 42
+    # x_topics, tf_idf_sparse = get_vectorized_sparse_matrix(term_doc_matrix, dictionary, optimal_number_of_topics)
+    # Creating sparse matrix
+    dct = Dictionary(prepared_text)
+    sparse = []
+    for topic in result.word_given_topic:
+        sparse.append([])
+        for word_tuple in topic:
+            sparse[-1].append(word_tuple[0])
+
+    # K-Means
+    cluster_labels = create_k_means_model(optimal_number_of_topics, sparse)
+    print(len(sparse), len(sparse[0]))
+    # plot_clusters_with_topics(x_topics, cluster_labels)
